ed95d3f751fdc36d02e0e5a520a1b434ce4f6a86
MMDaemon <mettenleitermatthias@web.de>
2018-12-12 14:50:29 +0100

    Added random Path generation

--- MoveOnGrid/Model.cs
+++ MoveOnGrid/Model.cs
@@ -1,5 +1,7 @@
-﻿using OpenTK;
+﻿using System;
+using OpenTK;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MoveOnGrid
 {
@@ -13,8 +15,8 @@ namespace MoveOnGrid
         public Vector2 WorldSize { get; private set; }
 
         private List<GameObject> _gameObjects;
-
         private GameObject[,] _world;
+        private Random _rand = new Random();
 
         public Model()
         {
@@ -22,6 +24,16 @@ namespace MoveOnGrid
             _gameObjects = new List<GameObject>();
             _world = new GameObject[(int)WorldSize.X, (int)WorldSize.Y];
 
+            InitiateWorld();
+        }
+
+        public void Update(float deltaTime)
+        {
+
+        }
+
+        private void InitiateWorld()
+        {
             for (int x = 0; x < WorldSize.X; x++)
             {
                 for (int y = 0; y < WorldSize.Y; y++)
@@ -30,11 +42,99 @@ namespace MoveOnGrid
                     _gameObjects.Add(_world[x, y]);
                 }
             }
+
+            CreatePath();
         }
 
-        public void Update(float deltaTime)
+        private void ClearWorld()
         {
+            for (int x = 0; x < WorldSize.X; x++)
+            {
+                for (int y = 0; y < WorldSize.Y; y++)
+                {
+                    if (_world[x, y].Type != 0)
+                    {
+                        SetType(new Vector2(x, y), 0);
+                    }
+                }
+            }
+        }
 
+        private void CreatePath()
+        {
+            Vector2 pos = new Vector2(_rand.Next(0, (int)WorldSize.X), 0);
+
+            SetType(pos, 1);
+
+            while (pos.Y < WorldSize.Y - 1)
+            {
+                pos = Step(pos);
+                if (pos == -Vector2.One)
+                {
+                    ClearWorld();
+                    CreatePath();
+                    break;
+                }
+                else
+                {
+                    SetType(pos, 1);
+                }
+            }
+        }
+
+        private Vector2 Step(Vector2 position)
+        {
+            List<Vector2> neighbors = GetNeighborPositions(position).ToList();
+
+            foreach (Vector2 neighbor in neighbors.ToArray())
+            {
+                if (_world[(int)neighbor.X, (int)neighbor.Y].Type == 1)
+                {
+                    neighbors.Remove(neighbor);
+                }
+                else
+                {
+                    IEnumerable<Vector2> nextNeighbors = GetNeighborPositions(neighbor);
+                    foreach (Vector2 nextNeighbor in nextNeighbors)
+                    {
+                        if (nextNeighbor != position && _world[(int)nextNeighbor.X, (int)nextNeighbor.Y].Type == 1)
+                        {
+                            neighbors.Remove(neighbor);
+                        }
+                    }
+                }
+            }
+
+            return neighbors.Count > 0 ? neighbors[_rand.Next(0, neighbors.Count)] : -Vector2.One;
+        }
+
+        private IEnumerable<Vector2> GetNeighborPositions(Vector2 pos)
+        {
+            List<Vector2> neighbors = new List<Vector2>();
+            if (pos.X + 1 < WorldSize.X)
+            {
+                neighbors.Add(pos + Vector2.UnitX);
+            }
+            if (pos.X - 1 >= 0)
+            {
+                neighbors.Add(pos - Vector2.UnitX);
+            }
+            if (pos.Y + 1 < WorldSize.Y)
+            {
+                neighbors.Add(pos + Vector2.UnitY);
+            }
+            if (pos.Y - 1 >= 0)
+            {
+                neighbors.Add(pos - Vector2.UnitY);
+            }
+            return neighbors;
+        }
+
+        private void SetType(Vector2 pos, int type)
+        {
+            _gameObjects.Remove(_world[(int)pos.X, (int)pos.Y]);
+            _world[(int)pos.X, (int)pos.Y] = new GameObject(type, pos, new Vector2(1));
+            _gameObjects.Add(_world[(int)pos.X, (int)pos.Y]);
         }
     }
 }
